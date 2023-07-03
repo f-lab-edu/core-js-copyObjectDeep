3d6ec6b6f39a8afe48ff061e1a6e6a9e63b028de
leejihoon <ljhljh211@naver.com>
2023-07-03 22:57:29 +0900

    fix: copyObjectDeep 함수 배열자료형 복사할 수 있게 개선

--- deepCopy.js
+++ deepCopy.js
@@ -1,11 +1,24 @@
-var copyObjectDeep = function (target) {
-  var result = {};
-  // 타겟이 오브젝트이면서 null이 아닐 때에만
-  if (typeof target === "object" && target !== null) {
+/**
+ *
+ * @param {any} target
+ * @returns
+ */
+var newCopyObjectDeep = function (target) {
+  var result;
+
+  // 배열일 경우 배열 자료형을 반환할 수 있도록 처리
+  if (Array.isArray(target)) {
+    result = [];
+    result = target.map((v) => {
+      return v;
+    });
+  } else if (typeof target === "object" && target !== null) {
+    result = {};
     for (var prop in target) {
-      result[prop] = copyObjectDeep(target[prop]); // 재귀함수?
+      result[prop] = newCopyObjectDeep(target[prop]); // 재귀함수?
     }
   } else {
+    console.log("target", target);
     result = target;
   }
   return result;
@@ -16,7 +29,9 @@ var obj = {
   b: { c: null, d: [1, 2] },
 };
 
-var obj2 = copyObjectDeep(obj);
+var obj2 = newCopyObjectDeep(obj);
 
 obj.b.c = "adf";
+obj.b.d = "edf";
+
 console.log(obj, obj2);
--- deepCopy.md
+++ deepCopy.md
@@ -1,18 +1,25 @@
 # Deep Copy 코어 자바스크립트 예제 보완
 
 이슈
-
 - 복사한 객체의 자료형이 달라지는 현상이 발생한다.
+  - 아래 책의 예제 코드에서 obj.b.d 의 자료형은 배열이지만
+  - 깊은 복사를 수행한 obj2.b.d 의 자료형은 객체이다.
+
+원인
+- copyObjectDeep 함수 안에서 `result` 변수로 빈 객체를 생성한 후에 복사한 값을 `result` 객체에 추가한 값을 반환받기 때문에 객체 타입으로 복사가 이루어진다.
+
 
 ```js
+// 예제1-17 코드
 var copyObjectDeep = function (target) {
   var result = {};
-  // 타겟이 오브젝트이면서 null이 아닐 때에만
   if (typeof target === "object" && target !== null) {
+    // 타겟이 오브텍느이면 copyObjectDeep 함수를 재귀적으로 호출
     for (var prop in target) {
       result[prop] = copyObjectDeep(target[prop]); // 재귀함수?
     }
   } else {
+    // 기본형이면 그냥 복사
     result = target;
   }
   return result;
@@ -31,5 +38,4 @@ console.log(obj, obj2);
 ```
 
 해결 방법
-
--
+- 자료형에 따라 복사한 객체도 동일한 자료형을 반환할 수 있도록 처리하기.
