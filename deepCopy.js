636ab9868a139c6096248d3db5e49d409b7672d2
leejihoon <ljhljh211@naver.com>
2023-07-05 22:51:52 +0900

    feat: 깊은복사 예제 코드 개선

---
+++ deepCopy.js
@@ -0,0 +1,37 @@
+  /**
+   *
+   * @param {any} target
+   * @returns
+   */
+  var newCopyObjectDeep = function (target) {
+    var result;
+
+    // 배열일 경우 배열 자료형을 반환할 수 있도록 처리
+    if (Array.isArray(target)) {
+      result = [];
+      result = target.map((v) => {
+        return v;
+      });
+    } else if (typeof target === "object" && target !== null) {
+      result = {};
+      for (var prop in target) {
+        result[prop] = newCopyObjectDeep(target[prop]); // 재귀함수?
+      }
+    } else {
+      console.log("target", target);
+      result = target;
+    }
+    return result;
+  };
+
+  var obj = {
+    a: 1,
+    b: { c: null, d: [1, 2] },
+  };
+
+  var obj2 = newCopyObjectDeep(obj);
+
+  obj.b.c = "adf";
+  obj.b.d = "edf";
+
+  console.log(obj, obj2);
